f36ab1fd7dda350be19f6d30624af9d6754ed1e3
Charlotte <charlotte.feather@yahoo.com>
2017-07-11 13:12:31 +0100

    Refactored the statement_body method

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,19 +1,23 @@
 class Statement
   def display(list_of_transactions, initial_balance)
     statement_text = "date || credit || debit || balance\n"
-    statement_text += statement_details(list_of_transactions,initial_balance)
+    statement_text += statement_body(list_of_transactions,initial_balance)
   end
 
 private
 
-  def statement_details(list_of_transactions, initial_balance)
+  def statement_body(list_of_transactions, initial_balance)
     previous_balance = initial_balance
     statement_text = ''
     list_of_transactions.each  do |transaction|
-      statement_text +=[transaction[0].strftime('%x'),credit_or_debit_column(transaction[1]), "#{transaction[1] + previous_balance}.00"].join(' || ') +"\n"
+      statement_text += statement_row(transaction, previous_balance)
       previous_balance += transaction[1]
     end
     statement_text
+  end
+
+  def statement_row(transaction, previous_balance)
+    [transaction[0].strftime('%x'),credit_or_debit_column(transaction[1]), "#{transaction[1] += previous_balance}.00"].join(' || ') +"\n"
   end
 
   def credit(value)
